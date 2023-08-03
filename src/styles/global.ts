3c141cd5d9345f7cc75be9e6a86ea40f0e5e71b2
Raul Santana <ti.raul77@gmail.com>
2023-08-03 20:39:53 -0300

    first commit

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,28 +1,40 @@
-import { globalCss } from "."
+import { globalCss } from "@stitches/react";
 
 export const globalStyles = globalCss({
-    '*':{
-        margin: 0,
-        padding: 0,
-        boxSizing: 'border-box',
+  "*": {
+    margin: 0,
+    padding: 0,
+
+    "::-webkit-scrollbar": {
+      width: 6,
     },
 
-    body:{
-        backgroundColor: "$gray900",
-        color: "$gray100",
-        '-webkit-font-smoothing':'antialiased',
+    "::-webkit-scrollbar-track": {
+      background: "$gray900",
     },
 
-    'body, input, textarea, button': {
-        fontFamily: 'Roboto',
-        fontWeight: 400,
+    "::-webkit-scrollbar-thumb": {
+      background: "$green500",
+      borderRadius: 10,
     },
+  },
 
-    a:{
-        color: 'inherit'
-    },
+  body: {
+    backgroundColor: "$gray900",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+  },
 
-    button: {
-        cursor: "pointer",
-    }
-})
+  "body, input, textarea, button": {
+    fontFamily: "Roboto",
+    fontWeight: 400,
+  },
+
+  a: {
+    color: "inherit",
+  },
+
+  button: {
+    cursor: "pointer",
+  },
+});
